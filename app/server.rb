8363d435ff9500ad7ce20076aff7273aad5e89d9
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-02-01 13:06:26 +0000

    Add client response to server.rb

--- app/server.rb
+++ app/server.rb
@@ -9,6 +9,8 @@ class YourRedisServer
     puts "Listening on port #{@port}..."
     server = TCPServer.new(@port)
     client = server.accept
+    client.recv(1024)
+    client.write("+PONG\r\n")
   end
 end
 

c8466206af839dde5417c4e6a04f6e1a55c49a3c
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-02-01 13:39:42 +0000

    Refactor database and server classes, add set_with_expiry method

--- app/database.rb
+++ app/database.rb
@@ -4,10 +4,27 @@ class Database
   end
 
   def set(key, value)
-    @data[key] = value
+    @data[key] = {
+      value: value,
+      expiry: nil
+    }
+  end
+
+  def set_with_expiry(key, value, expiry_in_milliseconds)
+    @data[key] = {
+      value: value,
+      expiry: Time.now + (expiry_in_milliseconds / 1000.0)
+    }
   end
 
   def get(key)
-    @data[key]
+    return nil unless @data[key]
+
+    if @data[key][:expiry] && @data[key][:expiry] < Time.now
+      @data.delete(key)
+      return nil
+    end
+
+    @data[key][:value]
   end
 end
--- app/server.rb
+++ app/server.rb
@@ -37,12 +37,32 @@ class RedisServer
       when "echo"
         client.write("$#{arguments[0].length}\r\n#{arguments[0]}\r\n")
       when "set"
+        option_arguments = []
+
+        if arguments.length > 2
+          option_arguments = arguments[2..]
+          arguments = arguments[0..1]
+        end
+
         key, value = arguments
-        @database.set(key, value)
-        client.write("+OK\r\n")
+
+        if option_arguments.empty?
+          @database.set(key, value)
+          client.write("+OK\r\n")
+        elsif option_arguments.first.eql?("ex") && option_arguments.length == 2
+          @database.set_with_expiry(key, value, option_arguments[1].to_i)
+          client.write("+OK\r\n")
+        else
+          client.write("-ERR unsupported SET options: #{option_arguments.join(" ")}\r\n")
+        end
       when "get"
         value = @database.get(arguments[0])
-        client.write("$#{value.length}\r\n#{value}\r\n")
+
+        if value.nil?
+          client.write("$-1\r\n")
+        else
+          client.write("$#{value.length}\r\n#{value}\r\n")
+        end
       else
         client.write("-ERR unknown command `#{command}`\r\n")
       end
